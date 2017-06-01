413453f677095412711d0f118324213131c44f7c
dlta <dlta.inc@icloud.com>
2017-05-31 22:41:25 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+console.log ('test');
+var answer;
+var question1;
+var question2;
+var question3;
+var question4;
+var question5;
+
+answer = Y || N;
+question1 = "Has Dianna been to Hawaii?";
+question2 = "Is Dianna a dog person?";
+question3 = "Is Dianna the youngest sibling?";
+question4 = "Does Dianna love reading?";
+question5 = "Does Dianna love seafood?";
+
+alert = 'Hi There!  Welcome to Dianna\'s About Me page.';
+alert = 'Before we get started, there will be a simple quiz to see how well you know Dianna, please answer using Y or N.'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow >20) {
+  greeting = 'Thank you for visiting, now go to bed!';
+} else {
+  greeting = 'Thank you for visiting, hope you have a great day!';
+}
+document.write ('<h3>' + greeting + '</h3>');
