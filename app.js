662eff828c0d4f3b7d144986a489efaae4ef033e
dlta <dlta.inc@icloud.com>
2017-06-06 21:12:28 -0700

    cleaned up all CSS and JS files

--- app.js
+++ app.js
@@ -1,50 +1,76 @@
 'use strict';
 
-console.log ('test');
+console.log ('Testing 1.2.3 Testing');
+alert ('Welcome Muggle!');
+
+var quizScore = 0;
+
+var guestName = prompt ('What\'s your name?');
+console.log (guestName);
+alert (guestName + ' before you start to learn more about Dianna let\'s quiz you on what you already know.  Remember to only answer with Y or N.');
 
 
-console.log ('test');
-
-
-var answer1 = Y;
-var answer2 = N;
-var question1;
-var question2;
-var question3;
-var question4;
-var question5;
-var correct = 5;
-
-question1 = "Has Dianna been to Hawaii?";
-question2 = "Is Dianna a dog person?";
-question3 = "Is Dianna the youngest sibling?";
-question4 = "Does Dianna love reading?";
-question5 = "Does Dianna love seafood?";
-
-var user = prompt('What is your name?');
-console.log('The user\'s name is: ' + user);
-
-alert = 'Hi ' + user '!  Welcome to Dianna\'s About Me page.';
-alert = 'Before we get started, there will be a simple quiz to see how well you know Dianna, please answer using Y or N.'
-
-var q1 = prompt(user + question1);
-if(q1.toUpperCase()==='Y') {
-  alert('Correct!  She visited the Aulani Resort!');
+var quizQ1 = prompt ('Does Dianna love the color purple?').toUpperCase();
+console.log(quizQ1);
+if (quizQ1 === 'Y') {
+    alert ('Ok ' + guestName + ' that was either a good guess, or you really know Dianna.  Let\'s proceed to the next question.');
+    quizScore++
+    console.log (quizScore);
 } else {
-  alert('AHH! Incorrect.')
+    alert ('Ahh not so correct ' + guestName + ', but at least now you\'ve learned Dianna loves the color purple.  Let\'s move on to the next question.');
 }
 
-
-
-
-
-
-var hourNow = today.getHours();
-var greeting;
-
-if (hourNow >20) {
-  greeting = 'Thank you for visiting, now go to bed!';
+var quizQ2 = prompt ('Does Dianna have more than 1 dog?').toUpperCase();
+console.log(quizQ2);
+if (quizQ2 === 'Y') {
+    alert (guestName + ' I\'m starting to think you might know Dianna, not like Facebook know her but for reals reals know her.  I\'m still not fully convinced though, answer the next few questions to confirm.');
+    quizScore++
+    console.log (quizScore);
 } else {
-  greeting = 'Thank you for visiting, hope you have a great day!';
+    alert (guestName + ', anyone who knows Dianna knows she has 2 dogs right now, but she\'s had as many as 5 at one time.');
 }
-document.write ('<h3>' + greeting + '</h3>');
+
+var quizQ3 = prompt ('Is Dianna fluent in 2 languages and learning others?').toUpperCase();
+console.log(quizQ3);
+if (quizQ3 === 'Y') {
+    alert ('Ok ' + guestName + ' I think we can both agree that was a bit of a gimmee.  Next question.');
+    quizScore++
+    console.log (quizScore);
+} else {
+    alert ('I don\'t know if your paying attention, that was a gimme.  The answer is yes, Dianna is fluent in 2 languages and learning more.');
+}
+
+var quizQ4 = prompt ('Is Dianna a fan of all things fantasy?').toUpperCase();
+console.log(quizQ4);
+if (quizQ4 === 'Y') {
+    alert (guestName + ' now I\'m convinced you really know Dianna.');
+    quizScore++
+    console.log (quizScore);
+} else {
+    alert ('Sad face!  She loves all things Harry Potter, Ms. Peregrines home for pecular children.  She prefers the book version over the movie any day of the week.');
+}
+
+var quizQ5 = prompt ('Is Dianna\'s favorite food sushi?').toUpperCase();
+console.log(quizQ5);
+if (quizQ5 === 'N') {
+    alert (guestName + ' your correct!  Everyone knows she loves pad thai with extra limes!  Weird I know right.');
+    quizScore++
+    console.log (quizScore);
+} else {
+    alert ('She is a fan of sushi, but it\'s not her favorite.  Pad thai with extra limes always wins.  Yes she acknowledges it\'s weird.');
+}
+
+var quizScoreResult = (quizScore/5*100);
+console.log (quizScoreResult + '%');
+
+document.write(guestName + ' Thank you for playing!  Your quiz score is ' + quizScoreResult + '%.  All the questions and answers to the quiz are as follows:');
+document.write(
+   "<ol>"
+     + "<li>" + 'Does Dianna love the color purple? YES!' + "</li>"
+     + "<li>" + 'Does Dianna have more than 1 dog? YES!' + "</li>"
+     + "<li>" + 'Is Dianna fluent in 2 languages and learning others? YES!' + "</li>"
+     + "<li>" + 'Is Dianna a fan of all things fantasy? YES!' + "</li>"
+     + "<li>" + 'Is Dianna\'s favorite food sushi? NO!' + "</li>"
+     +
+   "</ol>"
+);
